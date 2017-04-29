8c51ac5dfbeaf178d715b47ab72b4e9972c893b9
NicoMassa <nico.massa1995@gmail.com>
2017-04-29 15:30:37 +0200

    prova primo piatto bottone

--- app/src/main/java/com/example/appetizers/Home.java
+++ app/src/main/java/com/example/appetizers/Home.java
@@ -1,13 +1,20 @@
 package com.example.appetizers;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class Home extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        //setContentView(R.layout.activity_home);
+    }
+
+    public void vaiAlPiatto(View view){
+        Intent intent = new Intent(Home.this, PaginaPiatto.class);
+        startActivity(intent);
     }
 }
