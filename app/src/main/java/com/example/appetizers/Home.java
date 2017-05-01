06d751867dda1fa7bfdfc5309f7721bb9b89174a
NicoMassa <nico.massa1995@gmail.com>
2017-05-02 01:00:16 +0200

    Prova invio dati a Firebase

--- app/src/main/java/com/example/appetizers/Home.java
+++ app/src/main/java/com/example/appetizers/Home.java
@@ -5,12 +5,23 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Home extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        FirebaseDatabase database = null;
+        if(!FirebaseApp.getApps(this).isEmpty()) {
+            database = FirebaseDatabase.getInstance();
+            database.setPersistenceEnabled(true);
+        }
+        DatabaseReference myRef = database.getReference("message");
+        myRef.setValue("Costa!Costa!Costa!");
     }
 
     public void vaiAlPiatto(View view){
